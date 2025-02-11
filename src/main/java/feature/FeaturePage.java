5757971fd72790b609c7189ab82024a0ec6b0a7e
Jeevitha Jagadeesh <jeevithajagadeesh@Jeevithas-MacBook-Pro.local>
2025-02-11 22:43:37 +0000

    f2

--- src/main/java/feature/FeaturePage.java
+++ src/main/java/feature/FeaturePage.java
@@ -2,4 +2,17 @@ package feature;
 
 public class FeaturePage {
 
+	public static void f1() {
+		System.out.println("f1");
+	}
+
+	public static void f2() {
+		System.out.println("f2");
+
+	}
+
+	public static void main(String[] args) {
+	f1();
+	f2();
+	}
 }
